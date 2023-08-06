91bff9be4fcb13f545591a24975ecb3d101398cc
Ashika-18 <ashibasama@gmail.com>
2023-08-06 21:23:31 +0900

    form送信post_get

--- routes/hello.js
+++ routes/hello.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     var data = {
-        title: '–Hello!–',
-        content: 'これはサンプルのコンテンツですよ！<br>this is sample content.'
+        title: 'Hello!',
+        content: '何か書いて送信して下さい'
+    };
+    res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + '」と送信しましたね。'
     };
     res.render('hello', data);
 });
