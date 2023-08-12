5f6cdfd5fccca9f080563ac4875cf4b441f54ca7
Ashika <ashibasama@gmail.com>
2023-08-12 15:04:38 +0900

    データベース5-1完了

--- routes/hello.js
+++ routes/hello.js
@@ -3,23 +3,23 @@ const router = express.Router();
 
 const sqlite3 = require('sqlite3');
 
-//データオブジェクトの取得
-const db = new sqlite3.Database('mydb.db');
+const db = new sqlite3.Database('mydata.db');
 
-//GETアクセスの処理
+//getアクセスの処理
 router.get('/', (req, res, next) => {
     //データベースのシリアライズ
     db.serialize(() => {
         //レコードを全て取り出す
-       db.all("select * from mydata", (err, rows) => {
-        if (!err) {
-            var data = {
-                title: 'Hello!',
-                content: rows
-            };
-            res.render('hello', data);
-        }
-       });
+        db.all("select * from mydata", (err, rows) => {
+            //データベースアクセス完了時の処理
+            if (!err) {
+                var data = {
+                    title: 'Hello!',
+                    content: rows
+                };
+                res.render('hello', data);
+            }
+        });
     });
 });
 
