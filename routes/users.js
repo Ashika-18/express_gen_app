f8d192c2ed7f1942f2ee435d20bb53718e80b099
Ashika <ashibasama@gmail.com>
2023-08-15 13:51:05 +0900

    prismaのcreate(作成)処理まで完了

--- routes/users.js
+++ routes/users.js
@@ -49,4 +49,25 @@ router.get('/find', (req, res, next) => {
     });
 });
 
+//addの処理
+router.get('/add', (req, res, next) => {
+    const data = {
+        title: 'Users/Add',
+    }
+    res.render('users/Add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    prisma.User.create({
+        data:{
+            name: req.body.name,
+            pass: req.body.pass,
+            mail: req.body.mail,
+            age: +req.body.age
+        }
+    }).then(() => {
+        res.redirect('/users');
+    });
+});
+
 module.exports = router;
