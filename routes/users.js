8089bd688116c8d0671001a645e6e3f46df1de31
Ashika <ashibasama@gmail.com>
2023-08-15 14:50:55 +0900

    レコードの更新処理:完了

--- routes/users.js
+++ routes/users.js
@@ -70,4 +70,35 @@ router.post('/add', (req, res, next) => {
     });
 });
 
+//更新の処理
+router.get('/edit/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    prisma.user.findUnique(
+        { where: { id: +id } }
+    ).then(usr => {
+        const data = {
+            title: 'Users/Edit',
+            user: usr
+        };
+        res.render('users/edit', data);
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+    const { id, name, pass, mail, age } = req.body;
+
+    prisma.user.update({
+        where: { id: +id },
+        data: {
+            name:name,
+            mail: mail,
+            pass: pass,
+            age: +age
+        }
+    }).then(() => {
+        res.redirect('/users');
+    });
+});
+
 module.exports = router;
